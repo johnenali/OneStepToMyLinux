fddf1f629ea8ff2837a975d7a4478618c6a985c1
johnenali <dreamrider2011@gmail.com>
2013-08-04 12:08:32 +0800

    Update configMe.sh

--- configMe.sh
+++ configMe.sh
@@ -1,7 +1,20 @@
-#Install Software
+##Basic
+sudo apt-get install git vim
+##config
+git config --global core.editor "vim"
+git config --global user.name "enali"
+git config --global user.email "johnenali2011@gmail.com
+git config --global color.ui true
+git config --global credential.helper cache
+##vim
+git clone http://githut.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+####Install Software
 sudo apt-get install air-crackng w3af octave 
-sudo apt-get install guake zim
-#Config System
+sudo apt-get install guake zim git
+
+
+####Config System
 #.bashrc
 cat bashrc >> ~/.bashrc
 #wubi98
@@ -11,6 +24,8 @@ cd ibus-table-wubi
 ./configure --prefix=/usr --enable-wubi98 --disable-wubi86
 make
 sudo make install
+##git
+#config
 
 
 
