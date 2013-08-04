aacbd30a0d15b15d7e36a902bc552f013a16d9f2
johnenali <dreamrider2011@gmail.com>
2013-08-04 11:50:22 +0800

    Update configMe.sh

--- configMe.sh
+++ configMe.sh
@@ -1,2 +1,16 @@
 #Install Software
+sudo apt-get install air-crackng w3af octave 
+sudo apt-get install guake zim
 #Config System
+#.bashrc
+cat bashrc >> ~/.bashrc
+#wubi98
+cd ~/Dropbox/Software
+tar zxvf ibus-table-wubi.tar.gz
+cd ibus-table-wubi
+./configure --prefix=/usr --enable-wubi98 --disable-wubi86
+make
+sudo make install
+
+
+
